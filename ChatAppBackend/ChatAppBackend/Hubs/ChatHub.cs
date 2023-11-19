4bed71abeb7b7d954fd0fb5abe65d2947221980f
MatiasSalomon01 <matisalomond@gmail.com>
2023-11-19 17:39:12 -0300

    initial setup backend

---
+++ ChatAppBackend/ChatAppBackend/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppBackend.Hubs;
+
+public class ChatHub : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        await SendMessage("", "User connected!");
+        await base.OnConnectedAsync();
+    }
+
+    public async Task SendMessage(string name, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", name, message);
+    }
+}
