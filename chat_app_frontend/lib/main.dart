c3c0413b399eab3a31718a236dc5a0947fe2ccde
MatiasSalomon01 <matisalomond@gmail.com>
2023-11-20 22:39:07 -0300

    avences importantes

--- chat_app_frontend/lib/main.dart
+++ chat_app_frontend/lib/main.dart
@@ -32,20 +32,44 @@ class _HomeState extends State<Home> {
   late final HubConnection connection;
   List<String> messages = [];
   UniqueKey key = UniqueKey();
+  bool isChatRoom = false;
+  String userName = '';
+  final Map<String, List<String>> content = {};
+  final TextEditingController chatController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  @override
+  void dispose() {
+    chatController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     buildConnection();
     connection.onclose((exception) => onClosed);
-    connection.on('ReceiveMessage', receiveMessage);
+    connection.on(
+      'ReceiveMessage',
+      (arguments) async {
+        print(arguments);
+        for (var data in arguments!) {
+          if (content.containsKey(data["user"])) {
+            var value = data["message"].toString();
+            content[data["user"]] = [...content[data["user"]]!, value];
+          } else {
+            var keyValue = {
+              data["user"].toString(): [data["message"].toString()],
+            };
+            content.addAll(keyValue);
+          }
+        }
+        setState(() {});
+      },
+    );
   }
 
   buildConnection() {
-    connection = HubConnectionBuilder()
-        .withUrl(widget.serverUrl,
-            HttpConnectionOptions(logging: (level, message) => print(message)))
-        .build();
+    connection = HubConnectionBuilder().withUrl(widget.serverUrl).build();
   }
 
   void onClosed(Exception exception) => print('connection closed');
@@ -54,39 +78,194 @@ class _HomeState extends State<Home> {
     await connection.start();
   }
 
-  Future sendMessage(String message) async {
-    await connection.invoke('SendMessage', args: ['Matias $key', message]);
-  }
-
-  Future receiveMessage(dynamic message) async {
-    var value = message[1].toString();
-    messages.add(value);
-    print(value);
-    setState(() {});
+  Future sendAll(String message) async {
+    await connection.invoke('SendAll', args: [userName, message]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('CHAT APP'),
-            ...messages.map((e) => Text(e)).toList(),
-            ElevatedButton(
-              onPressed: () async {
-                sendMessage(UniqueKey().toString());
-              },
-              child: const Text('Send Message'),
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 29, 75, 62),
+        title: const Text(
+          'CHAT - APP',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.cloud_done_outlined),
-        onPressed: () async => await connect(),
-      ),
+      backgroundColor: const Color.fromARGB(255, 37, 36, 36),
+      body: isChatRoom
+          ? Container(
+              margin: const EdgeInsets.all(50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 50),
+                    child: Column(
+                      children: [
+                        ...content.keys
+                            .map(
+                              (key) => Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  // if (e != userName)
+                                  //   Text(
+                                  //     e.toString(),
+                                  //     style: const TextStyle(
+                                  //       color: Colors.purple,
+                                  //       fontSize: 14,
+                                  //     ),
+                                  //   ),
+                                  ...content[key]!.map(
+                                    (value) => Align(
+                                      alignment: key != userName
+                                          ? Alignment.bottomLeft
+                                          : Alignment.bottomRight,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(10),
+                                        margin: const EdgeInsets.symmetric(
+                                            vertical: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          color: key != userName
+                                              ? const Color.fromARGB(
+                                                  255, 43, 44, 44)
+                                              : const Color.fromARGB(
+                                                  255, 29, 75, 62),
+                                        ),
+                                        child: Text(
+                                          value.toString(),
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                            .toList()
+                      ],
+                    ),
+                  ),
+                  TextFormField(
+                    controller: chatController,
+                    focusNode: _focusNode,
+                    style: const TextStyle(color: Colors.white),
+                    cursorColor: Colors.grey,
+                    onEditingComplete: () {},
+                    onSaved: (newValue) {},
+                    onFieldSubmitted: (value) {
+                      sendAll(value);
+                      chatController.text = '';
+                    },
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 29, 75, 62),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: .5),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: .5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: .5),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: .5),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      width: 250,
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        cursorColor: Colors.grey,
+                        onFieldSubmitted: (value) async {
+                          setState(() => userName = value);
+                          await enterChat();
+                        },
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 29, 75, 62),
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: .5),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: .5),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: .5),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: .5),
+                          ),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 29, 75, 62),
+                      ),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(25)),
+                      shape: MaterialStateProperty.all(
+                        const StadiumBorder(
+                          side: BorderSide(color: Colors.white, width: .3),
+                        ),
+                      ),
+                    ),
+                    onPressed: () async => enterChat(),
+                    child: const Text(
+                      'Send Message',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       const Text('CHAT APP'),
+      //       ...messages.map((e) => Text(e)).toList(),
+      // ElevatedButton(
+      //   onPressed: () async {
+      //     sendMessage(UniqueKey().toString());
+      //   },
+      //   child: const Text('Send Message'),
+      // )
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: const Icon(Icons.cloud_done_outlined),
+      //   onPressed: () async => await connect(),
+      // ),
     );
+  }
+
+  Future enterChat() async {
+    setState(() => isChatRoom = true);
+    await connect();
   }
 }
