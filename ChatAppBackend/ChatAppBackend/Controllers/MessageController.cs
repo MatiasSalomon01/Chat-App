a51f786d63548e8dc73ad407d9af45fffe4cd86c
MatiasSalomon01 <matisalomond@gmail.com>
2023-11-24 21:30:54 -0300

    cambios varios

---
+++ ChatAppBackend/ChatAppBackend/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using ChatAppBackend.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Xml.Linq;
+
+namespace ChatAppBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+    private readonly IHubContext<ChatHub> _hub;
+
+    public MessageController(IHubContext<ChatHub> hub)
+    {
+        _hub = hub;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage(string name, string message)
+    {
+        await  _hub.Clients.All.SendAsync("ReceiveMessage", new { User = name, Message = message });
+        return Ok();
+    }
+}
