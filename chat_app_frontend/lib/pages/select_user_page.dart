3d7d32a0f7906579ce45e19eb9011f3c2cbddbdc
MatiasSalomon01 <matisalomond@gmail.com>
2023-12-10 21:14:46 -0300

    cambios varios

--- chat_app_frontend/lib/pages/select_user_page.dart
+++ chat_app_frontend/lib/pages/select_user_page.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app_frontend/constants/colors.dart';
+import 'package:chat_app_frontend/helpers/sized_box_helper.dart';
+import 'package:chat_app_frontend/models/user.dart';
 import 'package:chat_app_frontend/providers/supabase_provider.dart';
 import 'package:chat_app_frontend/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../widgets/profile_picture.dart';
 
 class SelectUserPage extends StatelessWidget {
   const SelectUserPage({super.key});
@@ -15,9 +19,9 @@ class SelectUserPage extends StatelessWidget {
       backgroundColor: secondary,
       appBar: AppBar(
         backgroundColor: primary,
-        title: Text(
-          'Iniciar sesión como:',
-          style: const TextStyle(color: white, fontSize: 20),
+        title: const Text(
+          'Iniciar sesión:',
+          style: TextStyle(color: white, fontSize: 20),
         ),
       ),
       body: FutureBuilder(
@@ -29,39 +33,78 @@ class SelectUserPage extends StatelessWidget {
           var data = snapshot.data!;
           provider.users = data;
           return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            padding: const EdgeInsets.all(15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Usuario disponibles',
-                  style: const TextStyle(color: white, fontSize: 17),
-                ),
-                ...data.map(
-                  (e) => TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(primary),
-                      overlayColor: MaterialStateProperty.all(secondary),
-                    ),
-                    onPressed: () {
-                      provider.myId = e.id;
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomeScreen(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      e.name,
-                      style: TextStyle(color: white, fontSize: 15),
-                    ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
                   ),
+                  itemBuilder: (context, index) =>
+                      _Item(provider: provider, user: data[index]),
                 ),
               ],
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _Item extends StatefulWidget {
+  const _Item({
+    required this.provider,
+    required this.user,
+  });
+
+  final SupabaseProvider provider;
+  final User user;
+
+  @override
+  State<_Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<_Item> {
+  bool onTap = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() => onTap = !onTap);
+        widget.provider.myId = widget.user.id;
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      },
+      onTapDown: (_) => setState(() => onTap = !onTap),
+      child: Container(
+        decoration: BoxDecoration(
+          color: white.withOpacity(onTap ? .1 : .03),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.user.name,
+              style: const TextStyle(color: white, fontSize: 16),
+            ),
+            verticalSpace(15),
+            ProfilePicture(
+              url: widget.user.profilePictureUrl,
+              radius: 40,
+            ),
+          ],
+        ),
       ),
     );
   }
