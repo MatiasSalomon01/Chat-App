3018c369986f391a4c86493f1ca62708ae101167
MatiasSalomon01 <matisalomond@gmail.com>
2023-12-16 21:32:47 -0300

    _GoBeginningButton

--- chat_app_frontend/lib/widgets/chat_messages.dart
+++ chat_app_frontend/lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:chat_app_frontend/extensions/datime_extensions.dart';
 import 'package:chat_app_frontend/models/chat_last_message.dart';
 import 'package:flutter/material.dart';
@@ -23,11 +24,23 @@ class _ChatMessagesState extends State<ChatMessages> {
   late Stream<dynamic> streamMessages;
   List<Message> messages = [];
   late SupabaseProvider provider;
+  late final ScrollController _controller;
+  bool showGoBeginning = false;
 
   @override
   void initState() {
     super.initState();
     provider = Provider.of<SupabaseProvider>(context, listen: false);
+    _controller = ScrollController();
+    _controller.addListener(() {
+      if (_controller.offset > kToolbarHeight) {
+        if (!showGoBeginning) setState(() => showGoBeginning = true);
+      }
+
+      if (_controller.offset < kToolbarHeight) {
+        if (showGoBeginning) setState(() => showGoBeginning = false);
+      }
+    });
     getStreamMessages();
   }
 
@@ -49,49 +62,92 @@ class _ChatMessagesState extends State<ChatMessages> {
           notification.disallowIndicator();
           return false;
         },
-        child: SingleChildScrollView(
-          reverse: true,
-          child: Column(
-            children: [
-              verticalSpace(5),
-              StreamBuilder(
-                stream: streamMessages,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(color: grey),
-                    );
-                  }
+        child: Stack(
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            SingleChildScrollView(
+              reverse: true,
+              controller: _controller,
+              child: Column(
+                children: [
+                  verticalSpace(5),
+                  StreamBuilder(
+                    stream: streamMessages,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(color: grey),
+                        );
+                      }
 
-                  messages = (snapshot.data! as List<dynamic>)
-                      .map((item) => Message.fromMap(item))
-                      .where((message) =>
-                          (message.senderId == provider.myId &&
-                              message.receiverId == widget.receiverId) ||
-                          (message.senderId == widget.receiverId &&
-                              message.receiverId == provider.myId))
-                      .toList();
+                      messages = (snapshot.data! as List<dynamic>)
+                          .map((item) => Message.fromMap(item))
+                          .where((message) =>
+                              (message.senderId == provider.myId &&
+                                  message.receiverId == widget.receiverId) ||
+                              (message.senderId == widget.receiverId &&
+                                  message.receiverId == provider.myId))
+                          .toList();
 
-                  messages = provider.putSeparators(messages);
+                      messages = provider.putSeparators(messages);
 
-                  if (messages.isEmpty) return Container();
-                  int itemCount = messages.length;
+                      if (messages.isEmpty) return Container();
+                      int itemCount = messages.length;
 
-                  return ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: itemCount,
-                    itemBuilder: (context, index) {
-                      final Message message = messages[index];
-                      return MessageBubble(
-                        message: message,
-                        isMe: message.senderId == provider.myId,
+                      return ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: itemCount,
+                        itemBuilder: (context, index) {
+                          final Message message = messages[index];
+                          return MessageBubble(
+                            message: message,
+                            isMe: message.senderId == provider.myId,
+                          );
+                        },
                       );
                     },
-                  );
-                },
+                  ),
+                ],
               ),
-            ],
+            ),
+            if (showGoBeginning)
+              _GoBeginningButton(onTap: () => _controller.jumpTo(0)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _GoBeginningButton extends StatelessWidget {
+  const _GoBeginningButton({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 5,
+      right: 15,
+      child: ZoomIn(
+        duration: const Duration(milliseconds: 200),
+        child: GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: primary,
+            ),
+            child: const Icon(
+              Icons.keyboard_double_arrow_down_rounded,
+              color: grey,
+              size: 20,
+            ),
           ),
         ),
       ),
